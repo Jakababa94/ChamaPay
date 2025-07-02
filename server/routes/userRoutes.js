d342388c9ababb7527af89b0746242f290a9c50e
Jakababa94 <jakababa002@gmail.com>
2025-07-02 15:04:09 +0300

    Admin access

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
+const { protect, authorize } = require('../middleware/auth');
 
 router.post('/register', UserController.register);
-router.get('/', UserController.getAll);
-router.get('/:id', UserController.getById);
-router.put('/:id', UserController.update);
-router.delete('/:id', UserController.delete);
+router.get('/', protect, authorize(['admin']), UserController.getAll);
+router.get('/:id', protect, authorize(['admin']), UserController.getById);
+router.put('/:id', protect, authorize(['admin']), UserController.update);
+router.delete('/:id', protect, authorize(['admin']), UserController.delete);
 
 module.exports = router; 
